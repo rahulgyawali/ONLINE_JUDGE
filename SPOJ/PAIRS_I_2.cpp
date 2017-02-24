b2636b686345c181497b3ec3a538d735e98c782d
Rahul Gyawali <rahulgyawali@users.noreply.github.com>
2017-02-24 23:56:50 +0530

    Update PAIRS_I_2.cpp

--- SPOJ/PAIRS_I_2.cpp
+++ SPOJ/PAIRS_I_2.cpp
@@ -1,3 +1,6 @@
+//FIND THE NO. OF PAIRS WHOSE DIFFERENCE IS EQUAL TO SUM
+//USED MAP
+
 #include <bits/stdc++.h>
 #include <algorithm>
 #include <iostream>
@@ -18,12 +21,13 @@ void findPair(long long int arr[], long long int n, long long int sum)
 	
     for (i = 0; i < n; i++) {
 	    
-        map[arr[i]] = i;
+        map[arr[i]] = i;					//Map the Values 
     }
  
     for (i = 0; i < n; i++) {    
 
-	count = count + map.count(arr[i]+sum);
+	count = count + map.count(arr[i]+sum);			//Count No.s whose value is Equal to ( No. + Sum )
+	   
     }
 
     cout <<count<<endl;
@@ -39,9 +43,11 @@ int main()
 	
 	long long int arr[n];	
 	
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++) {
+		
 	 	cin>>arr[i];
-
+	}
+	
 	findPair(arr, n, sum);
 	
  	return 0;
