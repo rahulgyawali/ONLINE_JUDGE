2e3e1fb8913b13d419e443791e88380d67dd5de7
Rahul Gyawali <rahulgyawali@users.noreply.github.com>
2018-02-08 14:05:25 +0530

    Easy,Priority_Queue

--- HACKER_EARTH/Max_in_Queue.cpp
+++ HACKER_EARTH/Max_in_Queue.cpp
@@ -3,21 +3,31 @@ using namespace std;
 int main()
 {
     long long int i,n,q,t,x;
+    
     cin>>n;
+    
     priority_queue<int> w;
+    
     while(n--) {
         cin>>x;
         w.push(x);
     }
+    
     cin>>q;
+    
     while(q--) {
+        
         cin>>t;
+        
         if(t== 1) {
-            cin>>x;
+            cin>>x; 
             w.push(x);
+            
         }else{
+            
             cout<<w.top()<<endl;
         }
     }
+    
     return 0;
 }
