f67dc416c7649069cdd739e86364524c5742e719
Rahul Gyawali <rahulgyawali@users.noreply.github.com>
2018-02-08 14:00:44 +0530

    Create Max_in_Queue.cpp

---
+++ HACKER_EARTH/Max_in_Queue.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int i,n,q,t,x;
+    cin>>n;
+    priority_queue<int> w;
+    while(n--) {
+        cin>>x;
+        w.push(x);
+    }
+    cin>>q;
+    while(q--) {
+        cin>>t;
+        if(t== 1) {
+            cin>>x;
+            w.push(x);
+        }else{
+            cout<<w.top()<<endl;
+        }
+    }
+    return 0;
+}
